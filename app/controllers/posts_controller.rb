efdd5a8ec30c29f0a476224822923715f1013a87
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 17:08:40 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 #updating index and show
 # updating posts controller because the resource we are requesting is related to posts
 # created a conditional for posts#index action because it renders a different sets of posts depending on the path.
-# rake routes 
+# rake routes
 # posts#index controller action = posts GET /posts url pattern 
   def index
     if params[:author_id] #if there is an :author_id in the params hash, user navigated to authors/:id/posts
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
     end
   end
 
-#posts#show will render the same information, data concerning a single post 
+#posts#show will render the same information, data concerning a single post
   def show
     @post = Post.find(params[:id])
   end
