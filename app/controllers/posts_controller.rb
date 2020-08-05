761f6c3062f177a7b9cc8369937c70639b38c45d
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 17:01:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 #updating index and show
 # updating posts controller because the resource we are requesting is related to posts
   def index
-    if params[:author_id] #if there is an :author_id in the params hash
+    if params[:author_id] #if there is an :author_id in the params hash, user navigated to authors/:id/posts
       @posts = Author.find(params[:author_id]).posts #user accessing index of a post by a certain author
     else
       @posts = Post.all #user accessing index of all posts
